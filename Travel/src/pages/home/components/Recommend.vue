96bde3265080768de664090082750410e1b1e18f
VincentEros <492269786@qq.com>
2018-09-08 20:07:40 +0800

    home complete

---
+++ Travel/src/pages/home/components/Recommend.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div class="title">❤猜你喜欢</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+          title: '上海迪士尼乐园',
+          desc: '随它吧,随它吧,这里永远是你的家'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1505/97/97d57e2d8cee926d.water.jpg_200x200_b000d264.jpg',
+          title: '澳门旅游观光塔',
+          desc: '澳门线上赌场,美女荷官在线发牌'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1408/28/511b24d8ca61752a581edbf8509865d4.jpg_200x200_5a6e3bc6.jpg',
+          title: '万神殿',
+          desc: '俯仰之间,灵魂临受万神的苛责'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/sight/p19/201211/08/133aa1fc1a44d1d493835fbb.jpg_200x200_25b16e00.jpg',
+          title: '卢浮宫',
+          desc: '多少年建成的,我已记不清了'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@styles/mixins.styl'
+  .title
+    color red
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height 1.9rem
+    .item-img
+      width: 1.7rem
+      height 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+    .item-title
+      line-height: .52rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color #ccc
+      ellipsis()
+    .item-button
+      line-height: .44rem
+      margin-top: .16rem
+      background: #ff9300
+      padding: 0 .1rem
+      border-radius .06rem
+      color: #fff;
+</style>
---
+++ Travel/src/pages/home/components/Weekend.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of weekendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg',
+          title: '上海必游TOP10',
+          desc: '中西合璧，现代和传统各有各的精彩'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1511/1d/fe098e8cfec117.jpg_r_640x214_a9be292a.jpg',
+          title: '上海城市观光',
+          desc: '来这里寻找你所认识的上海'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7b/2a6b0ce087f0e5.jpg_r_640x214_5c76b115.jpg',
+          title: '情迷博物馆',
+          desc: '穿梭时光岁月寻找答案'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/b3/671f666cc7845a.jpg_r_640x214_f977f30c.jpg',
+          title: '帆船游艇出海',
+          desc: '乘风破浪，直挂云帆济沧海'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@styles/mixins.styl'
+  .title
+    color black
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    height 0
+    overflow: hidden
+    padding-bottom: 33.9%;
+  .item-img
+    width: 100%
+  .item-info
+    padding: .1rem
+  .item-title
+    line-height: .52rem
+    font-size: .32rem
+    ellipsis()
+  .item-desc
+    line-height: .4rem
+    color #ccc
+    ellipsis()
+</style>
