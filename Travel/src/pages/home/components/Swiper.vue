5be822c0c4d402f64cb94dd50c7ade0e6ca3ebee
VincentEros <492269786@qq.com>
2018-09-08 09:20:46 +0800

    swiper

---
+++ Travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList">
+        <img class="swiper-img" :src="item.imgUrl" alt="" :key="item.id">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/ce/bca92895556c5002.jpg_750x200_cf56d46c.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/66/e5a5cec881702f02.jpg_750x200_67bb5691.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #ffffff
+  .wrapper
+    overflow hidden
+    width: 100%
+    height 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
--- Travel/src/pages/home/home.vue
+++ Travel/src/pages/home/home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
+import HomeSwiper from './components/Swiper'
 import HomeHeader from './components/Header'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
